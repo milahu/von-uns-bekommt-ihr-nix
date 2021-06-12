d7f551a5a4b544aa60ac8c67c23e00fbcda0d3f4
milahu <milahu@gmail.com>
2021-06-12 15:44:03 +0200

    scripts/git-add-remote.sh

---
+++ scripts/git-add-remote.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+git remote add github https://milahu@github.com/milahu/von-uns-bekommt-ihr-nix.git
+git remote add darktea http://milahu@it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/von-uns-bekommt-ihr-nix.git
