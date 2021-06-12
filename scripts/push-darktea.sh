1db6b04353d27924d595f8a2c4bea1b2bd6fd156
milahu <milahu@gmail.com>
2021-06-12 15:43:47 +0200

    scripts/push-darktea.sh

---
+++ scripts/push-darktea.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+torsocks git push -u darktea master
--- scripts/push-tor.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-false && {
-  git remote add darktea http://milahu@it7otdanqu7ktntxzm427cba6i53w6wlanlh23v5i3siqmos47pzhvyd.onion/milahu/von-uns-bekommt-ihr-nix.git
-}
-
-torsocks git push -u darktea master
